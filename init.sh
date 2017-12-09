1683b49639aec26de035474150e4751accbcd4a0
Dumitru Uzun <contact@duzun.me>
2017-12-09 20:33:01 +0200

    Copy .extend.* to ~ only one time

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # @author Dumitru Uzun (DUzun.Me)
-# @version 1.0.1
+# @version 1.1.0
 
 p="$(realpath `dirname $0`)/~"
 ln -sf "$p/.profile" ~/
@@ -10,7 +10,16 @@ ln -sf "$p/.curlrc" ~/
 ln -sf "$p/.vimrc" ~/
 
 ls -a "$p" | grep '^\.extend\.' | while read i; do
-    cat "$p/$i" >> ~/"$i";
+    _mark="#dotfiles: $i";
+    if [ -e ~/"$i" ] && grep "$_mark" ~/"$i" > /dev/null; then
+        echo "$i not updated"
+    else
+        echo "Updating $i ..."
+        echo "" >> ~/"$i";
+        cat "$p/$i" >> ~/"$i";
+        echo "" >> ~/"$i";
+        echo $_mark >> ~/"$i";
+    fi
 done
 
 . ~/.profile
