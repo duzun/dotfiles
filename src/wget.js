4afeaf8a241c46ee0acc16b32d22d1c73eb48354
Dumitru Uzun <contact@duzun.me>
2019-08-26 11:34:46 +0300

    Added wget.js fot download alias on Windows

---
+++ src/wget.js
@@ -0,0 +1,12 @@
+var WinHttpReq = new ActiveXObject("WinHttp.WinHttpRequest.5.1");
+WinHttpReq.Open("GET", WScript.Arguments(0), /*async=*/false);
+WinHttpReq.Send();
+WScript.Echo(WinHttpReq.ResponseText);
+
+/* To save a binary file use this code instead of previous line
+BinStream = new ActiveXObject("ADODB.Stream");
+BinStream.Type = 1;
+BinStream.Open();
+BinStream.Write(WinHttpReq.ResponseBody);
+BinStream.SaveToFile("out.bin");
+*/
